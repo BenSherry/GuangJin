e1596dac85a0a7fcd890c43d0e7cd9813fc4890a
jasoyang <yangchao61@outlook.com>
2024-01-25 19:02:30 +0800

    init java project

---
+++ java/bigbang/src/main/java/com/marvel/bigbang/BigbangApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.bigbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbangApplication.class, args);
+	}
+
+}
